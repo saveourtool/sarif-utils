50acd89263dfac14e051847491bb83b7c64fc3a5
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2023-01-10 12:36:11 +0300

    WIP ### What's done: * WIP

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,23 @@ rootProject.name = "sarifutils"
 
 include("fixpatches")
 
+plugins {
+    id("com.gradle.enterprise") version "3.9"
+}
+
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
     }
 }
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
