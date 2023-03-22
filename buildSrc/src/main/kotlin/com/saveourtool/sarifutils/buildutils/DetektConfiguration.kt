e13ada0c95fb5191730a2ee1ff90312cd2b06fbb
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2023-03-22 16:22:21 +0300

    Register task ### What's done: * Register task

--- buildSrc/src/main/kotlin/com/saveourtool/sarifutils/buildutils/DetektConfiguration.kt
+++ buildSrc/src/main/kotlin/com/saveourtool/sarifutils/buildutils/DetektConfiguration.kt
@@ -9,8 +9,10 @@ import io.gitlab.arturbosch.detekt.DetektPlugin
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 import io.gitlab.arturbosch.detekt.report.ReportMergeTask
 import org.gradle.api.Project
+import org.gradle.api.tasks.TaskProvider
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.named
 import org.gradle.kotlin.dsl.register
 import org.gradle.kotlin.dsl.withType
 
@@ -36,5 +38,17 @@ fun Project.createDetektTask() {
     }
     tasks.register("mergeDetektReports", ReportMergeTask::class) {
         output.set(buildDir.resolve("detekt-sarif-reports/detekt-merged.sarif"))
+    }
+
+    @Suppress("GENERIC_VARIABLE_WRONG_DECLARATION")
+    val reportMerge: TaskProvider<ReportMergeTask> = rootProject.tasks.named<ReportMergeTask>("mergeDetektReports") {
+        input.from(
+            tasks.withType<Detekt>().map { it.sarifReportFile }
+        )
+        shouldRunAfter(tasks.withType<Detekt>())
+    }
+    tasks.withType<Detekt>().configureEach {
+        reports.sarif.required.set(true)
+        finalizedBy(reportMerge)
     }
 }
